858db2a5720ccb49c246f0aebd6a49824dad9a08
Hendrik <drikdoank@gmail.com>
2017-07-16 13:59:43 +0700

    data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
 import { SegmentPage } from '../segment/segment';
+import { Platform } from 'ionic-angular';
+import { createConnection, ConnectionOptions } from 'ionic-orm'
+
 /**
  * Generated class for the HomePage page.
  *
@@ -16,14 +19,44 @@ import { SegmentPage } from '../segment/segment';
 
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
   }
 
-  transaksi(){
+  ionViewWillEnter() {
+    console.log("Home ionViewWillEnter");
+    this.platform.ready().then(() => {
+      let options: ConnectionOptions = {
+        autoSchemaSync: true,
+        driver: {
+          type: "mysql",
+          host: "localhost",
+          port: 3306,
+          username: "root",
+          password: "password",
+          database: "siapik"
+        },
+        logging: {
+          logQueries: true,
+          logSchemaCreation: true,
+          logFailedQueryError: true,
+          logOnlyFailedQueries: true
+        }
+      };
+      createConnection(options)
+        .then((connection) => {
+          console.log(`Connected Created ${connection}`);
+        })
+        .catch((error) => {
+          console.error(`Connection Error ${JSON.stringify(error)}`);
+        });
+    });
+  }
+
+  transaksi() {
     this.navCtrl.push(SegmentPage);
   }
 
