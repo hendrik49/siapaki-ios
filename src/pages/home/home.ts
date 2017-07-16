d8658922697436c580a5e827640db46f2a4cba46
Hendrik <drikdoank@gmail.com>
2017-07-16 16:48:51 +0700

    data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { TabsPage } from '../tabs/tabs';
 import { SegmentPage } from '../segment/segment';
 import { Platform } from 'ionic-angular';
-import { createConnection, ConnectionOptions } from 'ionic-orm'
 
 /**
  * Generated class for the HomePage page.
@@ -28,32 +26,6 @@ export class HomePage {
 
   ionViewWillEnter() {
     console.log("Home ionViewWillEnter");
-    this.platform.ready().then(() => {
-      let options: ConnectionOptions = {
-        autoSchemaSync: true,
-        driver: {
-          type: "mysql",
-          host: "localhost",
-          port: 3306,
-          username: "root",
-          password: "password",
-          database: "siapik"
-        },
-        logging: {
-          logQueries: true,
-          logSchemaCreation: true,
-          logFailedQueryError: true,
-          logOnlyFailedQueries: true
-        }
-      };
-      createConnection(options)
-        .then((connection) => {
-          console.log(`Connected Created ${connection}`);
-        })
-        .catch((error) => {
-          console.error(`Connection Error ${JSON.stringify(error)}`);
-        });
-    });
   }
 
   transaksi() {
